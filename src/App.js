762009a02c4d07b46cfb826dade4ee92e9043e08
Ghost <vincent.aipar@gmail.com>
2022-08-29 17:57:28 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const handleGetUser = () => {
+    fetch("https://https://simple-project-test-api.herokuapp.com//getUser")
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App works !</h1>
+      <button onClick={() => handleGetUser()}>Get User Infos</button>
     </div>
   );
 }
